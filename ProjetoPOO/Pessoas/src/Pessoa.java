a2fd499d3e0b3e3665b8f629063a91a7e3ec53d7
carolina1428 <119612742+carolina1428@users.noreply.github.com>
2023-12-11 09:43:31 +0000

    Update Pessoa.java

--- ProjetoPOO/Pessoas/src/Pessoa.java
+++ ProjetoPOO/Pessoas/src/Pessoa.java
@@ -5,92 +5,103 @@ import java.time.Year;
 import java.time.DateTimeException;
 import java.io.Serializable;
 
-public class Pessoa implements Serializable {
-	String nome;
-	String apelido;
-	int numescolar;
-	LocalDateTime nascimento;
+   public class Pessoa implements Serializable{
+	   String nome;
+	   String apelido;
+	   int numescolar;
+	   LocalDateTime nascimento;
+	   String cargo;	
+
 	
-	
-	public Pessoa(String nome, String apelido, int numescolar,LocalDateTime nascimento) {
+	public Pessoa(String nome, String apelido, int numescolar,LocalDateTime nascimento, String cargo) {
 		this.nome=nome;
 		this.apelido=apelido;
 		this.numescolar=numescolar;
 		this.nascimento=nascimento;
+		this.cargo=cargo;
 	}
-	  public static Pessoa novaPessoa() {
-	        System.out.print("O primeiro nome da pessoa: ");
-	        String nome = Ler.umaString();
-	        System.out.print("O apelido da pessoa: ");
-	        String apelido = Ler.umaString();
-	        int numescolar = Ler.umInt();
-	        System.out.println("Insira o seu numero escolar");
-	        
 
-	        LocalDateTime nascimento;
-	        while (true) {
-	            System.out.print("Data de nascimento\n");
-	            System.out.print("Dia: ");
-	            int dd = Ler.umInt();
+	public static Pessoa novaPessoa() {
+	    System.out.print("O primeiro nome da pessoa: ");
+	    String nome = Ler.umaString();
+	    System.out.print("O apelido da pessoa: ");
+	    String apelido = Ler.umaString();
+	    System.out.println("Insira o seu numero escolar");
+        int numescolar = Ler.umInt();
+        System.out.println("Insira o cargo: ");
+        String cargo = Ler.umaString();
 
-	            System.out.print("\nMes: ");
-	            int mm = Ler.umInt();
-	            Month month;
-	            try {
-	                month = Month.of(mm);
-	            } catch (DateTimeException e) {
-	                System.out.println("Mes invalido");
-	                continue;
-	            }
-	            System.out.print("\nAno: ");
-	            int yyyy = Ler.umInt();
+    LocalDateTime nascimento;
+    while (true) {
+        System.out.print("Data de nascimento\n");
+        System.out.print("Dia: ");
+        int dd = Ler.umInt();
 
-	            try {
-	                nascimento = LocalDateTime.of(yyyy, month, dd, 0, 0);
-	                break;
-	            } catch (DateTimeException e) {
-	                System.out.println("Data invalida!");
-	            }
-	        }
-	        return new Pessoa(nome, apelido ,numescolar, nascimento);
-	    }
-	
+        System.out.print("\nMes: ");
+        int mm = Ler.umInt();
+        Month month;
+        try {
+            month = Month.of(mm);
+        } catch (DateTimeException e) {
+            System.out.println("Mes invalido");
+            continue;
+        }
+        System.out.print("\nAno: ");
+        int yyyy = Ler.umInt();
+
+        try {
+            nascimento = LocalDateTime.of(yyyy, month, dd, 0, 0);
+            break;
+        } catch (DateTimeException e) {
+            System.out.println("Data invalida!");
+        }
+    }
+    return new Pessoa(nome, apelido ,numescolar, nascimento, cargo);
+}
+
 	public String getNome() {
-		return nome;
+	return nome;
 	}
 	public void setNome() {
-		this.nome=nome;
+	this.nome=nome;
 	}
 	public String getApelido() {
-		return apelido;
+	return apelido;
 	}
 	public void setApelido() {
-		this.apelido=apelido;
+	this.apelido=apelido;
 	}
 	
 	public int getNumescolar() {
-		return numescolar;
+	return numescolar;
 	}
 	public void setNumescolar(int numescolar) {
-		this.numescolar=numescolar;
+	this.numescolar=numescolar;
 	}
-	  public int getIdade() {
-	        Year nascimento = Year.of(this.nascimento.getYear());
-	        Year now = Year.now();
-	        return now.getValue() - nascimento.getValue();
-	    }
-
-	 public LocalDateTime getNascimento() {
-		 return nascimento;
-	 }
-
-	    public void setNascimento(LocalDateTime nascimento) {
-	        this.nascimento = nascimento;
-	    }
+	public int getIdade() {
+	    Year nascimento = Year.of(this.nascimento.getYear());
+	    Year now = Year.now();
+	    return now.getValue() - nascimento.getValue();
+	}
 	
+	public LocalDateTime getNascimento() {
+	 return nascimento;
+	}
+	
+	public void setNascimento(LocalDateTime nascimento) {
+	    this.nascimento = nascimento;
+	}
+	
+	public String getCargo() {
+		return cargo;
+	}
+	
+	public void setCargo(String cargo) {
+		this.cargo = cargo;
+	}
 	
 	public String tostring() {
-		String s = String.format("Nome: %s, Apelido: %s, Numeroescolar: %d, Idade: %d", nome, apelido, numescolar, getIdade());
-	    return s;
+	String s = String.format("Nome: %s, Apelido: %s, Numeroescolar: %d, Idade: %d, Cargo: %d", nome, apelido, numescolar, getIdade(), cargo);
+	return s;
 	}
 }
