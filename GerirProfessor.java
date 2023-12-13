2184cbe94049795721fa8aa4efddbf1c18212b30
carolina1428 <119612742+carolina1428@users.noreply.github.com>
2023-12-13 16:17:10 +0000

    Update GerirProfessor.java

--- GerirProfessor.java
+++ GerirProfessor.java
@@ -6,7 +6,7 @@ public class GerirProfessor {
         System.out.println("#########################################################");
         System.out.println("|                    Menu Professor                      |");
         System.out.println("|________________________________________________________|");
-        System.out.println("|                    1 - Adicionar Professo              |");
+        System.out.println("|                    1 - Adicionar Professor              |");
         System.out.println("|                    2 - Remover professor               |");
         System.out.println("|                    3 - Listar professores              |");
         System.out.println("|                    4 - Atualizar professor             |");
