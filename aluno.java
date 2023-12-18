bc24c934b7d13c514eeacd89c9402f40e4f47968
Bml2003 <152162852+Bml2003@users.noreply.github.com>
2023-12-18 10:29:21 +0000

    Create aluno.java

---
+++ aluno.java
@@ -0,0 +1,70 @@
+import java.util.ArrayList;
+
+import myinputs.Ler;
+
+import java.io.Serializable;
+import java.time.LocalDateTime;
+
+public class Aluno extends Pessoa {
+	    private Disciplina disciplina;
+	    private int NIF;
+        private String email;
+        private int anoescolar;
+        
+        public Aluno(String nome, String apelido, int numescolar, LocalDateTime nascimento, String disciplina, int nif, String email, int anoescolar) {
+            super(nome, apelido, numescolar, nascimento);
+            this.NIF = nif;
+            this.disciplina = new Disciplina(disciplina);
+            this.anoescolar= anoescolar;
+            
+    
+    }
+		    public Disciplina getDisciplina() {
+			return disciplina;
+		    }
+		    public void setDisciplina () {
+		    this.Disciplina=disciplina;	
+		    }
+			public int getNif() {
+				return NIF;
+			}
+			public void setNif(int NIF) {
+				this.NIF=NIF;
+			}
+			public String getEmail() {
+			     return email;
+			}
+			public void setEmail(String email) {
+			        this.email=email; 
+			}
+			public int getAnoescolar() {
+				return anoescolar;
+			}
+			public void setAnoescolar(int anoescolar) {
+					this.anoescolar=anoescolar;
+				}
+				
+			
+			
+			
+				
+			
+			
+			public static aluno novoAluno() {
+				Pessoa pessoa = novaPessoa();
+				
+				System.out.println("Insira a disciplina que frequenta: ");
+				String disciplina = Ler.umaString();
+
+				System.out.println("Insira o NIF do aluno: ");
+				int NIF = Ler.umInt();
+				
+				return new Aluno (pessoa.getNome(), pessoa.getApelido(), pessoa.getNumescolar(), pessoa.getNascimento(), disciplina , NIF);
+			}
+			
+			public String tostring(){
+				return super.toString() + String.format("Disciplina %s:, NIF %d:", disciplina, NIF ); 
+				
+			}
+		} 
+		
