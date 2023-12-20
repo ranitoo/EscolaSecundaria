389fde534faf2a314b163b2cf8fba128445c82ec
carolina1428 <119612742+carolina1428@users.noreply.github.com>
2023-12-20 12:05:25 +0000

    Update Aluno.java

--- Aluno.java
+++ Aluno.java
@@ -1,13 +1,11 @@
 import java.util.ArrayList;
-
 import myinputs.Ler;
-
 import java.io.Serializable;
 import java.time.LocalDateTime;
 
 public class Aluno extends Pessoa {
-	    private Disciplina disciplina;
-	    private int NIF;
+	private Disciplina disciplina;
+        private int NIF;
         private String email;
         private int anoescolar;
         
@@ -19,52 +17,52 @@ public class Aluno extends Pessoa {
             
     
     }
-		    public Disciplina getDisciplina() {
-			return disciplina;
-		    }
-		    public void setDisciplina () {
-		    this.Disciplina=disciplina;	
-		    }
-			public int getNif() {
-				return NIF;
+	    public Disciplina getDisciplina() {
+		return disciplina;
+	    }
+	    public void setDisciplina () {
+	    this.Disciplina=disciplina;	
+	    }
+		public int getNif() {
+			return NIF;
+		}
+		public void setNif(int NIF) {
+			this.NIF=NIF;
+		}
+		public String getEmail() {
+		     return email;
+		}
+		public void setEmail(String email) {
+			this.email=email; 
+		}
+		public int getAnoescolar() {
+			return anoescolar;
+		}
+		public void setAnoescolar(int anoescolar) {
+				this.anoescolar=anoescolar;
 			}
-			public void setNif(int NIF) {
-				this.NIF=NIF;
-			}
-			public String getEmail() {
-			     return email;
-			}
-			public void setEmail(String email) {
-			        this.email=email; 
-			}
-			public int getAnoescolar() {
-				return anoescolar;
-			}
-			public void setAnoescolar(int anoescolar) {
-					this.anoescolar=anoescolar;
-				}
 				
 			
-			
-			
-				
-			
-			
-			public static aluno novoAluno() {
-				Pessoa pessoa = novaPessoa();
-				
-				System.out.println("Insira a disciplina que frequenta: ");
-				String disciplina = Ler.umaString();
+						
+	
+	public static aluno novoAluno() {
+		Pessoa pessoa = novaPessoa();
+		
+		System.out.println("Insira a disciplina que frequenta: ");
+		String disciplina = Ler.umaString();
 
-				System.out.println("Insira o NIF do aluno: ");
-				int NIF = Ler.umInt();
-				
-				return new Aluno (pessoa.getNome(), pessoa.getApelido(), pessoa.getNumescolar(), pessoa.getNascimento(), disciplina , NIF);
-			}
+		System.out.println("Insira o NIF do aluno: ");
+		int NIF = Ler.umInt();
+
+		System.out.println("Insira o email do aluno: ");
+		String email = Ler.umString();
+		
+		return new Aluno (pessoa.getNome(), pessoa.getApelido(), pessoa.getNumescolar(), pessoa.getNascimento(), disciplina , NIF, email);
+	}
+	
+		public String tostring(){
+			return super.toString() + String.format("Disciplina: %s, NIF: %d, Email: %s", disciplina, NIF, email); 
 			
-			public String tostring(){
-				return super.toString() + String.format("Disciplina %s:, NIF %d:", disciplina, NIF ); 
-				
-			}
-		} 
+		}
+	} 
 		
