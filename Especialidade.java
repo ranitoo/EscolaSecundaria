4f069d590bea9f201351e79560a32bf02a9ca032
carolina1428 <119612742+carolina1428@users.noreply.github.com>
2023-12-23 17:21:22 +0000

    Update and rename Disciplina.java to Especialidade.java

---
+++ Especialidade.java
@@ -0,0 +1,62 @@
+import myinputs.Ler;
+import java.util.ArrayList;
+import java.util.List;
+
+public enum Especialidade {
+        Português(2000),
+        Biologia_e_Geologia(2000),
+        Matemática_A(2000),
+        Matemática_B(2000),
+        Filosofia(2000),
+        Economia(2000),
+        Geografia(2000),
+        Historia(2000),
+        Inglês(2000),
+        Espanhol(2000), 
+        Francês(2000),
+        Educação_Física(2000),
+        Oficina_de_Artes(2000),
+        TIC(2000),
+        Fisica_e_Quimica(2000);
+
+    private int salario;
+    
+    Especialidade(int salario) {
+        this.salario = salario;
+    }
+
+    public static List<Especialidade> getEspecialidades() {
+    	List<Especialidade> especialidadesSelecionadas = new ArrayList<>();
+    	
+    	System.out.println("Selecione as disciplinas que o professor leciona: ");
+    	int opcao;
+    	do {
+        System.out.println("         1 - Biologia e Geologia" +
+                "\n         2 -Matematica A " +
+                "\n         3 -Matematica B " +
+                "\n         4 - Portugues " +
+                "\n         5 - Filosofia" +
+                "\n         6 - Economia" +
+                "\n         7 - Geografia" +
+                "\n         8 - Historia" +
+                "\n         9 - Inglês" +
+                "\n         10 - Espanhol" +
+                "\n         11 - Francês" +
+                "\n         12 - Educação Fisica" +
+                "\n         13 - Oficina das Artes" +
+                "\n         14 - TIC" +
+                "\n         15 - Fisica e Quimica" +
+                "\n         0 - Finalizar seleção" +
+                "\n#########################################################" +
+                "\n                    Selecione uma opcao                  ");
+        opcao = Ler.umInt();
+        
+        if(opcao > 0 && opcao <= values().length) {
+        	especialidadesSelecionadas.add(values()[opcao - 1]);
+        } else if(opcao != 0) {
+        	System.out.println("Opção inválida.");
+        }
+    	}while (opcao != 0);
+        
+    	return especialidadesSelecionadas;
+    }}
