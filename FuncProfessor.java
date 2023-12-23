49d5f30e3ebb16254bd4c5e21e58ace50c536b90
carolina1428 <119612742+carolina1428@users.noreply.github.com>
2023-12-23 17:46:56 +0000

    Update FuncProfessor.java

--- FuncProfessor.java
+++ FuncProfessor.java
@@ -1,4 +1,49 @@
+import myinputs.Ler;
+import java.io.*;
+import java.util.ArrayList;
 
 public class FuncProfessor {
 
+    public static ArrayList<Professor> getProfessores() {
+        ArrayList<Professor> professores = new ArrayList<>();
+
+        try {
+            ObjectInputStream is = new ObjectInputStream(new FileInputStream("./src/MemoryFile/professor.dat"));
+            Professor.setUltimo(is.readInt());
+            professores = (ArrayList<Professor>) is.readObject();
+            is.close();
+        } catch (IOException | ClassNotFoundException e) {
+            System.out.println(e.getMessage());
+        }
+
+        return professores;
+    }
+
+    public static Professor searchProfessor(ArrayList<Professor> professores) {
+        System.out.print("Encontrar Professor por Nome ou Apelido: ");
+        String text = Ler.umaString();
+
+        for (Professor professor : professores) {
+            if (professor.getNome().contains(text) || professor.getApelido().contains(text)) {
+                System.out.print("Encontramos este professor: ");
+                System.out.println(professor);
+                return professor;
+            }
+        }
+
+        System.out.println("Não foram encontrados resultados!");
+        return null;
+    }
+
+    public static void saveToFile(ArrayList<Professor> professores) {
+        try {
+            ObjectOutputStream os = new ObjectOutputStream(new FileOutputStream("./src/MemoryFile/professor.dat"));
+            os.writeInt(Professor.getUltimo());
+            os.writeObject(professores);
+            os.flush();
+            os.close();
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
+    }
 }
