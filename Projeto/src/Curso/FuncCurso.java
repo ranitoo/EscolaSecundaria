824c6e9a7420809fed93a7a57874383b6858183c
ranitoo <95591333+ranitoo@users.noreply.github.com>
2023-12-27 22:14:32 +0000

    Update FuncCurso.java

--- Projeto/src/Curso/FuncCurso.java
+++ Projeto/src/Curso/FuncCurso.java
@@ -1,5 +1,43 @@
 package Curso;
 
+import myinputs.Ler;
+import java.io.*;
+import java.util.ArrayList;
+
 public class FuncCurso {
 
+    public static ArrayList<Curso> getCursos() {
+        ArrayList<Curso> cursos = new ArrayList<>();
+
+        try {
+            ObjectInputStream is = new ObjectInputStream(new FileInputStream("./src/Curso/curso.dat"));
+            cursos = (ArrayList<Curso>) is.readObject();
+            is.close();
+        } catch (IOException | ClassNotFoundException e) {
+            System.out.println(e.getMessage());
+        }
+
+        return cursos;
+    }
+
+    public static Curso getCursoPorNome(ArrayList<Curso> cursos, String nomeCurso) {
+        for (Curso curso : cursos) {
+            if (curso.getNome().equalsIgnoreCase(nomeCurso)) {
+                return curso;
+            }
+        }
+        return null;
+    }
+
+    public static void saveTofile(ArrayList<Curso> cursos) {
+        try {
+            ObjectOutputStream os = new ObjectOutputStream(new FileOutputStream("./src/Curso/curso.dat"));
+            os.writeObject(cursos);
+            os.flush();
+            os.close();
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
+    }
 }
+
