399c0302156b352bd7787a4977fac955ad471f2c
carolina1428 <119612742+carolina1428@users.noreply.github.com>
2023-12-28 18:08:47 +0000

    Update GerirCurso.java

--- Projeto/src/Curso/GerirCurso.java
+++ Projeto/src/Curso/GerirCurso.java
@@ -1,9 +1,7 @@
 package Curso;
-
-
-import Professor.Disciplina;
 import Aluno.Aluno;
 import Professor.Professor;
+import Disciplina.Disciplina;
 import myinputs.Ler;
 import java.util.ArrayList;
 import java.util.List;
@@ -15,10 +13,11 @@ public class GerirCurso {
         System.out.println("#########################################################");
         System.out.println("|                    Menu Curso                          |");
         System.out.println("|________________________________________________________|");
-        System.out.println("|                    1 - Inscrever Aluno                |");
-        System.out.println("|                    2 - Atribuir Professor             |");
-        System.out.println("|                    3 - Gerir Horários                 |");
-        System.out.println("|                    7 - Sair                            |");
+        System.out.println("|                    1 - Adicionar Cursos                |");
+        System.out.println("|                    2 - Inscrever Aluno                 |");
+        System.out.println("|                    3 - Atribuir Professor              |");
+        System.out.println("|                    4 - Gerir Horários                  |");
+        System.out.println("|                    5 - Sair                            |");
         System.out.println("#########################################################");
         System.out.println("                    Selecione uma opcao                  ");
         opcao = Ler.umInt();
@@ -32,78 +31,123 @@ public class GerirCurso {
         do {
             op = menu();
             switch (op) {
-                case 1:
-                    inscreverAluno(cursos);
-                    break;
-
+            	case 1:
+            		adicionarCurso(cursos);
+            		 FuncCurso.saveTofile(cursos); 
+            		break;
+            		
                 case 2:
-                    atribuirProfessor(cursos);
+                    inscreverAluno(cursos);    
+                    FuncCurso.saveTofile(cursos); 
                     break;
 
                 case 3:
-                    gerirHorarios(cursos);
+                    atribuirProfessor(cursos);  
+                    FuncCurso.saveTofile(cursos); 
                     break;
-            }
-        } while (op != 7);
 
-        FuncCurso.saveTofile(cursos); // Movido para fora do loop do menu
+                case 4:
+                     //chamar função da classe gerirhorarios 
+                	break;
+                
+            }
+            FuncCurso.saveTofile(cursos);
+        } while (op != 5);
+ 
+       
     }
     
+    
+    private static void adicionarCurso(List<Curso> cursos) {
+        System.out.println("Digite o nome do novo curso: ");
+        String nomeCurso = Ler.umaString();
 
+        
+        ArrayList<Disciplina> disciplinas = atribuirDisciplinasAoCurso();
+
+        Curso novoCurso = new Curso(nomeCurso);
+        cursos.add(novoCurso);
+
+        System.out.println("Novo curso adicionado com sucesso!");
+        FuncCurso.saveTofile(cursos);
+    }
+
+    
     private static void inscreverAluno(List<Curso> cursos) {
-        // Implemente a lógica para inscrever um aluno em um curso
-        // Exemplo:
+    	System.out.println("Cursos disponíveis: ");
+    	for(Curso curso : cursos) {
+    		System.out.println(curso.getNome());
+    	}
+
         System.out.println("Digite o nome do curso: ");
         String nomeCurso = Ler.umaString();
         Curso curso = FuncCurso.getCursoPorNome(cursos, nomeCurso);
 
         if (curso != null) {
-            curso.inscreverAluno(Aluno.novoAluno());
+        	
+            Aluno aluno = Aluno.novoAluno();
+            curso.inscreverAluno(aluno);
+            FuncCurso.saveTofile(cursos); 
             System.out.println("Aluno inscrito com sucesso no curso " + nomeCurso);
         } else {
             System.out.println("Curso não encontrado.");
         }
+        FuncCurso.saveTofile(cursos); 
     }
 
+    
     private static void atribuirProfessor(List<Curso> cursos) {
-        // Implemente a lógica para atribuir um professor a um curso
-        // Exemplo:
         System.out.println("Digite o nome do curso: ");
         String nomeCurso = Ler.umaString();
         Curso curso = FuncCurso.getCursoPorNome(cursos, nomeCurso);
 
-        if (curso != null) {
             Professor professor = Professor.novoProfessor();
             System.out.println("Digite o nome da disciplina: ");
             String nomeDisciplina = Ler.umaString();
-            Disciplina disciplina = new Disciplina(nomeDisciplina);
 
-            curso.atribuirProfessor(professor, disciplina);
-            System.out.println("Professor atribuído com sucesso à disciplina " + nomeDisciplina + " no curso " + nomeCurso);
-        } else {
-            System.out.println("Curso não encontrado.");
+            Disciplina disciplina = getDisciplinaPorNome(nomeDisciplina);
+
+                curso.atribuirProfessor(professor, disciplina);
+                System.out.println("Professor atribuído com sucesso à disciplina " + nomeDisciplina + " no curso " + nomeCurso);   
         }
-    }
-
-    private static void gerirHorarios(List<Curso> cursos) {
-        // Implemente a lógica para gerir horários em um curso
-        // Exemplo:
-        System.out.println("Digite o nome do curso: ");
-        String nomeCurso = Ler.umaString();
-        Curso curso = FuncCurso.getCursoPorNome(cursos, nomeCurso);
-
-        if (curso != null) {
-            System.out.println("Digite o nome da disciplina: ");
-            String nomeDisciplina = Ler.umaString();
-            Disciplina disciplina = new Disciplina(nomeDisciplina);
-
-            System.out.println("Digite o novo horário para a disciplina " + nomeDisciplina + ": ");
-            String novoHorario = Ler.umaString();
-
-            curso.gerirHorarios(disciplina, novoHorario);
-            System.out.println("Horário gerenciado com sucesso para a disciplina " + nomeDisciplina + " no curso " + nomeCurso);
-        } else {
-            System.out.println("Curso não encontrado.");
+    
+    
+    private static Disciplina getDisciplinaPorNome(String nomeDisciplina) {
+        for (Disciplina disciplina : Disciplina.values()) {
+            if (disciplina.name().equalsIgnoreCase(nomeDisciplina)) {
+                return disciplina;
+            }
         }
-    }
+        return null;
+      }
+
+
+private static ArrayList<Disciplina> atribuirDisciplinasAoCurso() {
+    ArrayList<Disciplina> disciplinasSelecionadas = new ArrayList<>();
+    int opcao;
+
+    do {
+        System.out.println("Escolha uma disciplina para adicionar ao curso:");
+        List<Disciplina> disciplinaList = Disciplina.getDisciplinas();
+        System.out.println("Disciplinas disponíveis:");
+        for (int i = 0; i < disciplinaList.size(); i++) {
+            System.out.println((i + 1) + " - " + disciplinaList.get(i));
+        }
+        System.out.println("0 - Finalizar seleção");
+
+        opcao = Ler.umInt();
+
+        if (opcao > 0 && opcao <= disciplinaList.size()) {
+            disciplinasSelecionadas.add(disciplinaList.get(opcao - 1));
+            System.out.println("Disciplina adicionada: " + disciplinaList.get(opcao - 1));
+        } else if (opcao != 0) {
+            System.out.println("Opção inválida.");
+        }
+
+    } while (opcao != 0);
+  
+
+    return disciplinasSelecionadas;
+   
+}
 }
