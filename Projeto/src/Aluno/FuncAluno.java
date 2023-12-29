b90a12f5266ba2cb20a94d59d47b20f0ac16112b
carolina1428 <119612742+carolina1428@users.noreply.github.com>
2023-12-29 17:58:28 +0000

    Update FuncAluno.java

--- Projeto/src/Aluno/FuncAluno.java
+++ Projeto/src/Aluno/FuncAluno.java
@@ -1,40 +1,41 @@
 package Aluno;
+import java.io.*;
 import java.util.ArrayList;
-
-import myinputs.Ler;
+import java.util.List;
 
 public class FuncAluno {
-	public static void listarAlunos(ArrayList<Aluno> alunos) {
-	    if (alunos.isEmpty()) {
-	        System.out.println("Não há alunos inscritos.");
-	    } else {
-	        System.out.println("Lista de Alunos Inscritos:");
-	        for (Aluno aluno : alunos) {
-	            System.out.println(aluno.toString());
-	        }
-	    }
-	}
 
-	public static void removerAluno(ArrayList<Aluno> alunos, int numescolar) {
-		boolean removed = false;
-		for (int i = 0; i < alunos.size(); i++) {
-			if(alunos.get(i).getNumescolar() == numescolar) {
-				alunos.remove(i);
-				removed = true;
-				break;
-			}}
-		
-		if (removed) {
-			System.out.println("Aluno removido com sucesso!");
-		}else {
-			System.out.println("Aluno não encontrado.");
-		}
-	}
-	
-	
-    public static void adicionarAluno(ArrayList<Aluno> alunos) {
-        Aluno novoAluno = Aluno.novoAluno(); // Chama o método estático novoAluno da classe Aluno para criar um novo aluno
-        alunos.add(novoAluno); // Adiciona o novo aluno à lista de alunos
-        System.out.println("Aluno adicionado com sucesso!");
+    public static ArrayList<Aluno> getAlunos() {
+        ArrayList<Aluno> alunos = new ArrayList<Aluno>();
+
+        try {
+            ObjectInputStream is = new ObjectInputStream(new FileInputStream("./src/Aluno/aluno.dat"));
+            alunos = (ArrayList<Aluno>) is.readObject();
+            is.close();
+        } catch (IOException | ClassNotFoundException e) {
+            System.out.println(e.getMessage());
+        }
+
+        return alunos;
+    }
+
+    public static Aluno getAlunoPorNumeroEscolar(List<Aluno> alunos, int numeroEscolar) {
+        for (Aluno aluno : alunos) {
+            if (aluno.getNumescolar() == numeroEscolar) {
+                return aluno;
+            }
+        }
+        return null;
+    }
+
+    public static void saveToFile(List<Aluno> alunos) {
+        try {
+            ObjectOutputStream os = new ObjectOutputStream(new FileOutputStream("./src/Aluno/aluno.dat"));
+            os.writeObject(alunos);
+            os.flush();
+            os.close();
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
     }
 }
