df28ae0911fcfb2c7abb240d7127161c66e70de1
carolina1428 <119612742+carolina1428@users.noreply.github.com>
2023-12-29 13:15:38 +0000

    Update FuncEscola.java

--- Projeto/src/Escola/FuncEscola.java
+++ Projeto/src/Escola/FuncEscola.java
@@ -1,5 +1,36 @@
 package Escola;
 
+
+import java.io.*;
+import java.util.ArrayList;
+
 public class FuncEscola {
 
+    public static ArrayList<Escola> getEscolas() {
+        ArrayList<Escola> escolas = new ArrayList<>();
+
+        try {
+            ObjectInputStream is = new ObjectInputStream(new FileInputStream("./src/Escola/escola.dat"));
+            Escola.setUltimoId(is.readInt());
+            escolas = (ArrayList<Escola>) is.readObject();
+            is.close();
+        } catch (IOException | ClassNotFoundException e) {
+            System.out.println(e.getMessage());
+        }
+
+        return escolas;
+    }
+
+    public static void saveToFile(ArrayList<Escola> escolas) {
+        try {
+            ObjectOutputStream os = new ObjectOutputStream(new FileOutputStream("./src/Escola/escola.dat"));
+            os.writeInt(Escola.getUltimoId());
+            os.writeObject(escolas);
+            os.flush();
+            os.close();
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
+    }
 }
+
