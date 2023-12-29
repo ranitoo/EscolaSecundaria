a1727e0c1b7e72bc45589a43243f38ef8e97b669
carolina1428 <119612742+carolina1428@users.noreply.github.com>
2023-12-29 17:11:39 +0000

    Update Horarios.java

--- Projeto/src/Horario/Horarios.java
+++ Projeto/src/Horario/Horarios.java
@@ -1,20 +1,25 @@
-package Horario;
+package Horarios;
+import java.io.Serializable;
 import myinputs.Ler;
-import java.util.ArrayList;
-import java.time.LocalTime	;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
 
-public class Horarios {
+public class Horarios implements Serializable{
 	private static int ultimoId = 0;
 	private int id;
 	private String diasemana;
+	private String turma;
+	private String disciplina;
 	LocalTime horas;
-	private ArrayList<Horarios>horarios;
 	
 	
-	public Horarios(String diasemana,LocalTime horas,int id) {
+	public Horarios(String diasemana,LocalTime horas,String disciplina, String turma) {
+		this.id = ultimoId + 1;
+		ultimoId++;
 		this.diasemana=diasemana;
-		this.id=id;	
+		this.disciplina = disciplina;
 		this.horas=horas;
+		this.turma= turma;
 	}
 	public static int getUltimo() {
         return ultimoId;
@@ -35,44 +40,60 @@ public class Horarios {
 	public void setdiasemana(String diasemana) {
 		this.diasemana=diasemana;
 	}
+	public String getDisciplina() {
+		return disciplina;
+	}
+	public void setDisciplina(String disciplina) {
+		this.disciplina = disciplina;
+	}
 	public LocalTime getHoras() {
 		return horas;
 	}
+	
+    public String getTurma() {
+        return turma;
+    }
+    
+    public void setTurma(String turma) {
+    	this.turma = turma; 
+    }
 	public void setHoras(LocalTime horas) {
         this.horas = horas;
     }
 	
 	
+	public static Horarios NovoHorario() {
+        System.out.println("\nAdicionar Disciplina ao Horario");
+        String dia;
+        do {
+            System.out.print("Dia da semana: ");
+            dia = Ler.umaString();
+        } while(!dia.equals("Segunda") && !dia.equals("Terça") && !dia.equals("Quarta") && !dia.equals("Quinta") && !dia.equals("Sexta"));
 
-	 private static void criarNovoHorario() {
-	        System.out.println("\nAdicionar Disciplina ao Horario");
-	        String dia;
-			// Solicitar informações do usuário
-	        do {
-	            System.out.print("Dia da semana: ");
-	            dia = Ler.umaString();
-	        } while (!dia.equals("Segunda") && !dia.equals("Terça") && !dia.equals("Quarta") && !dia.equals("Quinta") && !dia.equals("Sexta"));
-	        
-	        System.out.print("Hora de inicio: ");
-	        int horaInicio = Ler.umInt();
+        System.out.print("Turma: ");
+        String turma = Ler.umaString();
 
-	        System.out.print("Hora de termino: ");
-	        int horaFim = Ler.umInt();
+        System.out.print("Hora de inicio: ");
+        int horaInicio = Ler.umInt();
 
-	        System.out.print("Disciplina: ");
-	        String disciplina = Ler.umaString();
+        System.out.print("Hora de termino: ");
+        int horaFim = Ler.umInt();
 
-	        // Criar uma representação do horário e adicioná-lo à lista
-	        String novoHorario;
-	        novoHorario = dia + ": " + disciplina + "(" + horaInicio + " - " + horaFim + ")";
-	          
-	        //Horarios.add(novoHorario);
+        System.out.print("Disciplina: ");
+        String disciplina = Ler.umaString();
 
-	        System.out.println("Nova disciplina adicionada");
-	    }
+        return new Horarios(dia, LocalTime.of(horaInicio, horaFim), disciplina, turma);
+    }
+	
+	
+    public String toString() {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("HH:mm");
 
-	    private static void visualizarHorarios() {
-	        
-	    }
+        return String.format("Horario [Turma: " + turma +
+               ", Dia: " + diasemana +
+               ", Disciplina: " + disciplina +
+               ", Turma: " + turma +
+               ", Horas: " + horas.format(formatter) +             
+               "]");
+    }
 }
-
