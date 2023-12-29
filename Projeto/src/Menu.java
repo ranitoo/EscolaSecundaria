e65bb8c628bedaa258658d65ae6773d9e77afb5a
carolina1428 <119612742+carolina1428@users.noreply.github.com>
2023-12-29 17:59:41 +0000

    Update Menu.java

--- Projeto/src/Menu.java
+++ Projeto/src/Menu.java
@@ -1,38 +1,45 @@
-import myinputs.Ler;
+
 import Aluno.GerirAluno;
+import Curso.GerirCurso;
+import Escola.GerirEscola;
 import Professor.GerirProfessor;
-import Horario.GerirHorario;
-import Aluno.GerirAluno;
+import myinputs.Ler;
 
 public class Menu {
-    public static void main(String[] args){
-    	int poo=0;
-        do{
-            System.out.println("#########################################################");
-            System.out.println("|                    Menu Principal                      |");
-            System.out.println("|________________________________________________________|");
-            System.out.println("|                    1 - Gerir Alunos                    |");
-            System.out.println("|                    2 - Gerir Professor                 |");
-            System.out.println("|                    3 - Gerir horários                  |");
-            System.out.println("|                    0 - Sair                            |");
-            System.out.println("#########################################################");
-            System.out.println("                    Selecione uma opcao                  ");
-            
-            poo = Ler.umInt();
-            switch (poo) {
-                case 1:
-                	GerirAluno.gerirAluno();
-                    break;
-              
-                case 2:
-                	GerirProfessor.gerirProfessor();
-                    break;
-                case 3:
-                	GerirHorario.gerirHorarios();
-                    break;
-                
-            }
-        }
-        while(poo != 0);
-    }
+	
+	 public static void main(String[] args){
+	    	int op=0;
+	        do{
+	            System.out.println("#########################################################");
+	            System.out.println("|                    Menu Principal                      |");
+	            System.out.println("|________________________________________________________|");
+	            System.out.println("|                    1 - Gerir Alunos                    |");
+	            System.out.println("|                    2 - Gerir Professores               |");
+	            System.out.println("|                    3 - Gerir Cursos                    |");
+	            System.out.println("|                    4 - Gerir Escola                    |");
+	            System.out.println("|                    0 - Sair                            |");
+	            System.out.println("#########################################################");
+	            System.out.println("                    Selecione uma opcao                  ");
+	            
+	            op = Ler.umInt();
+	    
+	            switch (op) {
+	                case 1:
+	                	GerirAluno.gerirAluno();
+	                    break;
+	                case 2:
+	                	GerirProfessor.gerirProfessor();
+	                    break;
+	                case 3:
+	                	GerirCurso.gerirCurso();
+	                    break;
+	                case 4:
+	                	GerirEscola.gerirEscola();
+	                	break;
+	                
+	            }
+	        }
+	        while(op != 0);
+	    }
 }
+
