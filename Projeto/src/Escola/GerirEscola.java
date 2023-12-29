d8c5995364ab9555934a21f5296dd97bd4978107
carolina1428 <119612742+carolina1428@users.noreply.github.com>
2023-12-29 13:16:20 +0000

    Update GerirEscola.java

--- Projeto/src/Escola/GerirEscola.java
+++ Projeto/src/Escola/GerirEscola.java
@@ -1,5 +1,71 @@
 package Escola;
 
-public class GerirEscola {
+import myinputs.Ler;
+import java.util.ArrayList;
+import java.util.List;
+import Curso.Curso;
+import Curso.FuncCurso;
+import Curso.GerirCurso;
+import Professor.FuncProfessor;
+import Professor.Professor;
 
-}
+public class GerirEscola {
+    public static int menu() {
+        int opcao;
+        System.out.println("#########################################################");
+        System.out.println("|                    Menu Escola                        |");
+        System.out.println("|_______________________________________________________|");
+        System.out.println("|                    1 - Curso mais frequentado         |");
+        System.out.println("|                    2 - Média de Idade por Curso       |");
+        System.out.println("|                    3 - Calcular Ranking               |");
+        System.out.println("|                    4 - Sair                           |");
+        System.out.println("#########################################################");
+        System.out.println("                    Selecione uma opcao                  ");
+        opcao = Ler.umInt();
+        return opcao;
+    }
+    
+
+        public static void gerirEscola() {
+            Escola minhaEscola = new Escola("Minha Escola");
+            ArrayList<Escola> escolas = FuncEscola.getEscolas();
+            
+            int opcao;
+
+            do {
+                opcao = menu();
+                switch (opcao) {
+                    case 1:
+                    	cursoMaisFrequentado(minhaEscola);
+                        break;
+
+                    case 2:
+                        mediaIdadePorCurso(minhaEscola);
+                        break;
+
+                    case 3:
+                        calcularRanking(minhaEscola);
+                        break;
+                }
+                FuncEscola.saveToFile(escolas);
+            } while (opcao != 4);
+        }
+
+
+    public static void cursoMaisFrequentado(Escola escola) {
+        Curso cursoMaisFrequentado = escola.cursoMaisFrequentado();
+        System.out.println("curso com mais alunos: " + cursoMaisFrequentado);
+    }
+
+ /*   public static void mediaIdadePorCurso(Escola escola) {
+        escola.mediaIdadesPorCurso();
+    }
+    */
+    public static void mediaIdadePorCurso(Escola escola) {
+        double mediaIdade = escola.mediaIdadesPorCurso(); // Chamando a função da classe Escola
+        System.out.println("Média de idade dos alunos em todos os cursos: " + mediaIdade);
+    }
+    
+    public static void calcularRanking(Escola escola) {
+        escola.calcularRanking();
+    }}
