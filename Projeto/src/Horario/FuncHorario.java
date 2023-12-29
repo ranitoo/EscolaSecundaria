2c95c60a0557be7c529696122931fda66cd444d7
carolina1428 <119612742+carolina1428@users.noreply.github.com>
2023-12-29 17:44:21 +0000

    Update FuncHorario.java

--- Projeto/src/Horario/FuncHorario.java
+++ Projeto/src/Horario/FuncHorario.java
@@ -1,19 +1,25 @@
-package Horario;
+package Horarios;
 import myinputs.Ler;
-
 import java.io.FileInputStream;
+import java.time.LocalTime;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 import java.util.ArrayList;
+import java.util.List;
 
-import Horario.Horarios;
+import Professor.Professor;
 
-public class FuncHorario {
+import java.util.Iterator;
+
+public class FuncHorario {	
+    
 	public static ArrayList<Horarios> gethorarios(){
 		 ArrayList<Horarios> horarios = new ArrayList<Horarios>();
 
 	        try {
-	            ObjectInputStream is = new ObjectInputStream(new FileInputStream("C:\\Users\\grani\\Desktop\\ubi\\ProjetoPOO\\Projeto\\src\\Horario\\horario.dat\\"));
+	            ObjectInputStream is = new ObjectInputStream(new FileInputStream("./src/Horarios/horarios.dat"));
 	            Horarios.setUltimo(is.readInt());
 	            horarios = (ArrayList<Horarios>) is.readObject();
 	            is.close();
@@ -22,6 +28,21 @@ public class FuncHorario {
 	        }
 
 	        return horarios;
+	}
+	
+	   
+
+	public static void Guardar(ArrayList<Horarios> horarios) {
+	    try (
+	    	ObjectOutputStream os = new ObjectOutputStream(new FileOutputStream("./src/Horarios/horarios.dat"))) {
+	    	os.writeInt(Horarios.getUltimo());
+            os.writeObject(horarios);
+            os.flush();
+            os.close();	                   
+	    } catch (IOException e) {
+	        System.err.println("Erro ao guardar os horários" + e.getMessage());
 	    }
 	}
 	
+
+}
